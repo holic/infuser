4a1291bcee82f1f7184318b8c499c3b1c3f4a1e5
Kevin Ingersoll <kingersoll@gmail.com>
2012-11-27 20:10:31 -0800

    Inject script tag instead of eval
    Using eval loads the script within Chrome's sandbox, denying access to window, etc. objects set by the page.

--- extension/injector.js
+++ extension/injector.js
@@ -2,21 +2,18 @@
 
 	var domain = this.location.hostname.replace(/^www\./, ''),
 		js = 'https://localhost:3131/' + domain + '.js';
-		
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', js, true);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState != 4) return;
-		if (xhr.status != 200) return console.error('[injector][%s] Failed to load script, is Adblock enabled?', domain);
-		try {
-			eval(xhr.responseText);
-			console.log('[injector][%s] Loaded script %s', domain, js);
-		}
-		catch (e) {
-			console.trace()
-			console.error('[injector][%s] Failed to load script %s\n\n', domain, js, e.stack)
-		}
+	
+	var script = document.createElement('script'),
+		parent = document.head || document.documentElement || document.body;
+	script.src = js;
+	script.async = true;
+	script.onload = function() {
+		this.parentNode.removeChild(this);
+		console.log('[injector][%s] Loaded script %s', domain, js);
 	};
-	xhr.send();
+	script.onerror = function() {
+		console.error('[injector][%s] Failed to load script %s\n\tIs the server running?\n\tIs AdBlock preventing the script from loading?', domain, js);
+	};
+	parent.appendChild(script);
 
 }).call(this);
